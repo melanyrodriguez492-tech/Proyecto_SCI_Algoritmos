849a0c98a4b79e6f48d175c206067d735632e98a
leonardochavez <leonardo.chavroj@gmail.com>
2025-11-24 23:47:50 -0500

    Codigo base y completamente funcional terminado, falta optimizar y las 'recomendaciones a los usuarios'

--- SistemaSeguridadApp/Biblioteca/Class1.cs
+++ SistemaSeguridadApp/Biblioteca/Class1.cs
@@ -9,10 +9,24 @@ namespace Biblioteca
 {
     public class biblioteca
     {
+
+
+
         int opcion;
         bool estado_alarma_parpadeante = false;
         bool estado_alarma = false;
+
+        //MENSAJE DE ENCABEZADO
+        string mensaje_fijo = ("Sistema SCI // Monitoreando...");
+        
         string[] mensaje = new string[6];
+
+
+
+
+        //NO BORRAR VACIOS (ORDEN VISUAL)
+
+
 
         // MENU PRINCIPAL
         public void menu()
@@ -27,12 +41,21 @@ namespace Biblioteca
                 Console.WriteLine("        PANEL DE CONTROL SCI - GRUPO '4'   ");
                 Console.WriteLine("=============================================");
 
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"\n--- ESTADO: NORMAL ---");
+                if (estado_alarma)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n*** ESTADO: ALARMA ACTIVADA! ***");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\n--- ESTADO: NORMAL ---");
+                }
                 Console.ForegroundColor = ConsoleColor.White;
 
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"\nMENSAJE: {mensaje[0]}");
+                Console.WriteLine($"\nMENSAJE: {mensaje_fijo}");
+
                 Console.ForegroundColor = ConsoleColor.White;
 
                 Console.WriteLine("\nSeleccione la acción a simular:");
@@ -42,9 +65,10 @@ namespace Biblioteca
                 Console.WriteLine("4. Simular Falla de Sensor (Aviso de Mantenimiento)");
                 Console.WriteLine("5. RESETEAR Sistema (Apagar Alarma)");
                 Console.WriteLine("6. Salir");
-                Console.Write("Seleccione una opción: ");
+                Console.Write("\nSeleccione una opción: ");
 
                 opcion = int.Parse(Console.ReadLine());
+                Console.Clear();
 
                 switch (opcion)
                 {
@@ -59,28 +83,94 @@ namespace Biblioteca
                             activar_alarma();
                             Console.ForegroundColor = ConsoleColor.Yellow;
                             Console.WriteLine($"\nMENSAJE: {mensaje[1]}");
+                            mensaje_fijo = mensaje[1];
                             Console.ForegroundColor = ConsoleColor.White;
                         }
                         break;
+                    case 2:
+                        if (estado_alarma)
+                        {
+                            comprobar_alarma();
+                        }
+                        else
+                        {
+                            post_seleccion();
+                            activar_alarma();
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"\nMENSAJE: {mensaje[2]}");
+                            mensaje_fijo = mensaje[2];
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    case 3:
+                        if (estado_alarma)
+                        {
+                            comprobar_alarma();
+                        }
+                        else
+                        {
+                            post_seleccion();
+                            activar_alarma();
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"\nMENSAJE: {mensaje[3]}");
+                            mensaje_fijo = mensaje[3];
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    case 4:
+                        if (estado_alarma)
+                        {
+                            comprobar_alarma();
+                        }
+                        else
+                        {
+                            post_seleccion();
+                            activar_alarma();
 
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"\nMENSAJE: {mensaje[4]}");
+                            mensaje_fijo = mensaje[4];
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
                     case 5:
                         estado_alarma = false;
                         estado_alarma_parpadeante = false;
-                        Console.WriteLine("Sistema reseteado");
-                        break;
+                        Console.ForegroundColor = ConsoleColor.Blue;
 
+                        Console.WriteLine("\n=============================================");
+                        Console.WriteLine("       REINICIANDO SISTEMA (Modo Normal)   ");
+                        Console.WriteLine("=============================================");
+
+                        Console.ForegroundColor = ConsoleColor.White;
+                        mensaje_fijo = mensaje[0];
+                        break;
                     case 6:
-                        Console.WriteLine("Saliendo del programa...");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\n==============================================");
+                        Console.WriteLine("           SALIENDO DEL PROGRAMA...   ");
+                        Console.WriteLine("==============================================");
+                        Console.ForegroundColor = ConsoleColor.White;
                         valor_bool = false;
+                        break;
+                    default:
+                        Console.WriteLine("\nEscoja una opción valida");
                         break;
                 }
 
-                Console.WriteLine("\nPresione ENTER para continuar...");
-                Console.ReadLine();
+                Console.WriteLine("\n\nPresione ENTER para continuar...");
+                Console.ReadKey();
                 estado_alarma_parpadeante = false;
                 Console.Clear();
             }
         }
+
+
+
+
+        //NO BORRAR VACIOS (ORDEN VISUAL)
+
+
 
         // PANTALLA DE ALARMA
         public void post_seleccion()
@@ -95,6 +185,17 @@ namespace Biblioteca
             Console.ForegroundColor = ConsoleColor.White;
         }
 
+
+
+
+
+        //NO BORRAR VACIOS (ORDEN VISUAL)
+
+
+
+
+
+
         // CAMBIA ESTADO DE ALARMA
         public void comprobar_alarma()
         {
@@ -104,9 +205,21 @@ namespace Biblioteca
             }
             else
             {
-                Console.WriteLine("El sistema ya está con ALARMA ACTIVA.");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n==============================================");
+                Console.WriteLine("     El sistema ya está con ALARMA ACTIVA   ");
+                Console.WriteLine("==============================================");
+                Console.ForegroundColor = ConsoleColor.White;
             }
         }
+
+
+
+
+        //NO BORRAR VACIOS (ORDEN VISUAL)
+
+
+
 
         // SIRENA HORIZONTAL (CHATGPT)
         public void alarma(int lineaSirena)
@@ -128,11 +241,25 @@ namespace Biblioteca
                 Console.Write(new string(' ', barra.Length));
 
                 Thread.Sleep(intervalo);
+
+                Console.Beep(1500, 120);
+                Console.Beep(100, 500); 
+                
             }
 
             Console.ResetColor();
             Console.CursorVisible = true;
         }
+
+
+
+
+
+        //NO BORRAR VACIOS (ORDEN VISUAL)
+
+
+
+
 
         // ACTIVAR ALARMA (CHATGPT)
         public void activar_alarma()
@@ -153,20 +280,26 @@ namespace Biblioteca
             Console.ForegroundColor = ConsoleColor.White;
         }
 
+
+
+
+        //NO BORRAR VACIOS (ORDEN VISUAL)
+
+
+
+
         //MENSAJES
         public void mensaje_opciones()
         {
 
             mensaje[0] = "Sistema SCI // Monitoreando...";
-            mensaje[1] = "HUMOOOO";
-            mensaje[2] = "TEMPERATURA CRITICA";
-            mensaje[3] = "ESTACION MANUAL";
-            mensaje[4] = "FALLO DE SENSOR";
+            mensaje[1] = "ALARMA DE HUMO DETECTADA en Zona de Turbina A.";
+            mensaje[2] = "ALARMA DE TEMPERATURA DETECTADA en Zona de Generador Auxiliar.";
+            mensaje[3] = " ALARMA DE ESTACIÓN MANUAL DETECTADA en Pasillo de Emergencia.";
+            mensaje[4] = "Sensor de Humo no responde en Zona 1. Contacte a mantenimiento";
             mensaje[5] = "Sistema SCI // Monitoreando...";
 
         }
-
     }
-
 }
 
